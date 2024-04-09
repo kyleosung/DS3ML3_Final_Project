a3babc70a91b0a2c87121a6cf6b9fbcf0d218bc1
Kyle Sung <97492761+kyleosung@users.noreply.github.com>
2024-04-09 19:55:31 -0400

    Update splitter.sh

--- scripts/splitter.sh
+++ scripts/splitter.sh
@@ -1,8 +1,8 @@
-mkdir ../../Data/DataTrain
-tail -n +2 ../../Data/Jan_Data_Parsed.csv | split -l 100000 - ../../Data/DataTrain/Chess_Jan_
-for file in ../../Data/DataTrain/Chess_Jan_*
+mkdir ../Data/DataTrain
+tail -n +2 ../Data/Jan_Data_Parsed.csv | split -l 100000 - ../Data/DataTrain/Chess_Jan_
+for file in ../Data/DataTrain/Chess_Jan_*
 do
-    head -n 1 ../../Data/Jan_Data_Parsed.csv > ../../Data/DataTrain/tmp_file
-    cat $file >> ../../Data/DataTrain/tmp_file
-    mv -f ../../Data/DataTrain/tmp_file $file
+    head -n 1 ../Data/Jan_Data_Parsed.csv > ../Data/DataTrain/tmp_file
+    cat $file >> ../Data/DataTrain/tmp_file
+    mv -f ../Data/DataTrain/tmp_file $file
 done
